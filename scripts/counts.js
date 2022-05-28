21cfe8c942763472a26d6ab584388743efcc05ef
luyane luiz <luyane.luiz.ll@gmail.com>
2022-05-28 12:17:38 -0300

    feat: criação campo trends e promo day

---
+++ scripts/counts.js
@@ -0,0 +1,7 @@
+let secounds = 60;
+
+function timer() {
+  secounds--;
+}
+console.log(timer());
+// document.querySelector("[secound]").innerHTML = secounds;
---
+++ scripts/trends.js
@@ -0,0 +1,9 @@
+const span = document.querySelector("span");
+
+function Scroll() {
+  const news = document.querySelector(".news");
+  const hots = document.querySelector(".hots");
+
+  news.classList.toggle("hidden");
+  hots.classList.toggle("hidden");
+}
