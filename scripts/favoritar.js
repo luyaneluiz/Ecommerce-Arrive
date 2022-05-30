aca17604ff037b491908cf7de19df23c0c865464
luyane luiz <luyane.luiz.ll@gmail.com>
2022-05-30 16:50:45 -0300

    feat: elaboração estrutura de produtos e footer

---
+++ scripts/favoritar.js
@@ -0,0 +1,12 @@
+function favoritarOn(e) {
+  e.setAttribute("type", "solid");
+}
+
+function favoritarOff(e) {
+  e.setAttribute("type", "");
+  e.setAttribute("name", "heart");
+}
+
+function favoritarTrue(e) {
+  e.setAttribute("type", "solid");
+}
