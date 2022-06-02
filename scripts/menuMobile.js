6b7c22f11957d86f695a58a9576cac55546f8fdc
luyane luiz <luyane.luiz.ll@gmail.com>
2022-06-02 16:27:25 -0300

    core: andamento do botão de expansão de categorias menu mobile

--- scripts/menuMobile.js
+++ scripts/menuMobile.js
@@ -1,30 +1,40 @@
+const navigator = document.querySelector(".navigator-desktop");
+const btnMoreCategories = document.querySelector(".btn-more-categories");
+const dropCategories = document.querySelector(".menu-drop");
+const categories = document.querySelector(".categories");
+
 function menuMobileOn() {
-  const navigator = document.querySelector(".navigator-desktop");
-  const divBtn = document.querySelector(".btn-mobile");
-  const btnClose = document.createElement("box-icon");
-
-  //criar botão de fechar menu
-  btnClose.setAttribute("name", "x"); //adicionar o atributo name correspondente ao nome do icone
-
-  divBtn.appendChild(btnClose); //adicionando o filho btnClose no navigator
-  navigator.appendChild(divBtn); //adicionando o filho btnClose no navigator
-  navigator.insertAdjacentElement("afterbegin", divBtn); //definindo o btnClose como o primeiro filho do navigator
-
-  //adicionar ou retirar classe de menu ativo conforme clique
   navigator.classList.toggle("active");
-
-  //criar botão de ver mais categorias
-  const moreCategories = document.querySelector(".more-categories");
-  const btnMoreCategories = document.querySelector(".btn-more-categories");
-
   btnMoreCategories.setAttribute("name", "plus");
+  // dropCategories.classList.remove("active");
 
-  moreCategories();
+  categories.addEventListener("click", moreCategories());
+
+  closeButton();
 }
 
 function moreCategories() {
-  const dropCategories = document.querySelector(".menu-drop");
+  // dropCategories.classList.toggle("active");
+  // btnMoreCategories.setAttribute("name", "minus");
 
-  dropCategories.classList.toggle("active");
+  // if (dropCategories.classList.contains("active")) {
+  //   btnMoreCategories.setAttribute("name", "minus");
+  // } else {
+  //   btnMoreCategories.setAttribute("name", "plus");
+  // }
+
   console.log("funfou");
+}
+
+function closeButton() {
+  const btnClose = document.querySelector(".close-btn");
+
+  btnClose.setAttribute("name", "x");
+  btnClose.addEventListener("click", closeMenu);
+
+  function closeMenu() {
+    navigator.classList.remove("active");
+  }
+
+  // dropCategories.classList.remove("active");
 }
