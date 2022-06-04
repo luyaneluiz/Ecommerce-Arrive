892c8b8519dbe276ece3ee68dc23092703e1f210
luyane luiz <luyane.luiz.ll@gmail.com>
2022-06-04 17:07:38 -0300

    feat: transcrição do código Main para CSS BEM

--- scripts/trends.js
+++ scripts/trends.js
@@ -1,8 +1,8 @@
 const span = document.querySelector("span");
 
 function Scroll() {
-  const news = document.querySelector(".news");
-  const hots = document.querySelector(".hots");
+  const news = document.querySelector(".trends__news");
+  const hots = document.querySelector(".trends__hots");
 
   news.classList.toggle("hidden");
   hots.classList.toggle("hidden");
